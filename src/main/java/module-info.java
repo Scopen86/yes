2d40fb85013283dec3d48e09e5d16918befa0308
scopen <scopen86@gmail.com>
2025-10-05 13:26:12 +0700

    update renderer (temporary)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
-module org.example.demofx {
+module org.example.arkanoidfxp {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.kordamp.ikonli.javafx;
 
-    opens org.example.demofx to javafx.fxml;
-    exports org.example.demofx;
+    opens org.example.arkanoidfxp to javafx.fxml;
+    exports org.example.arkanoidfxp;
 }
